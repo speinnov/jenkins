dd25e76e89e779faeb7b45015b10421aa3c4fe78
SPEINNOV <36667485+speinnov@users.noreply.github.com>
2018-10-27 10:35:54 +0200

    commandons service sh file

---
+++ commandite/commandLine_startservice.sh
@@ -0,0 +1,10 @@
+# Orginal
+#./Talend-Studio-macosx-cocoa.app/Contents/MacOS/Talend-Studio-macosx-cocoa -nosplash -application org.talend.commandline.CommandLine -data commandline-workspace startServer -p 8002 -vmargs -Djava.awt.headless=true -Dawt.toolkit=sun.awt.HToolkit -Xms64m -Xmx768m -XstartOnFirstThread -Dorg.eclipse.swt.internal.carbon.smallFonts
+#./Talend-Studio-macosx-cocoa.app/Contents/MacOS/Talend-Studio-macosx-cocoa  -nosplash -application org.talend.commandline.CommandLine -data /Users/hadoop/talend_builder/Talend-CI-Builder-20170623_1246-V6.4.1/commandline_workspace startServer -p 8002   -vmargs -Djava.awt.headless=true  -Dawt.toolkit=sun.awt.HToolkit -Xms64m -Xmx1024m -XstartOnFirstThread   -Dorg.eclipse.swt.internal.carbon.smallFonts
+# 1 workspace
+#./Talend-Studio-macosx-cocoa.app/Contents/MacOS/Talend-Studio-macosx-cocoa  --disableShellInput   -nosplash -application org.talend.commandline.CommandLine -consoleLog -data /Users/hadoop/github_repo⁩/jenkins⁩ startServer -p 8002   -vmargs -Djava.awt.headless=true  -Dawt.toolkit=sun.awt.HToolkit  -Xms64m -Xmx1024m -XstartOnFirstThread   -Dorg.eclipse.swt.internal.carbon.smallFonts
+# ./Talend-Studio-linux-gtk-x86 --disableShellInput -nosplash   execute  commandline with & in background
+#/Users/hadoop/talend_builder/commandline_workspace1
+#/Users/hadoop/talend_builder/Talend-CI-Builder-20170623_1246-V6.4.1/JENKINS_HOME/workspace/talend_job_pipeline
+# 2 commandline_workspace1 test
+./Talend-Studio-macosx-cocoa.app/Contents/MacOS/Talend-Studio-macosx-cocoa  --disableShellInput   -nosplash -application org.talend.commandline.CommandLine -consoleLog -data /Users/hadoop/talend_builder/commandline_workspace1  startServer -p 8002   -vmargs -Djava.awt.headless=true  -Dawt.toolkit=sun.awt.HToolkit  -Xms64m -Xmx1024m -XstartOnFirstThread   -Dorg.eclipse.swt.internal.carbon.smallFonts
